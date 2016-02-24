934cddae143c189fe710980c34e18d2a174c8522
Benjamin Chohon <bchohon@eharmony.com>
2016-02-23 16:04:33 -0800

    updated partial link

--- static/js/templates.js
+++ static/js/templates.js
@@ -15,7 +15,7 @@ templates['projects'] = template({"1":function(depth0,helpers,partials,data) {
   if (stack1 != null) { buffer += stack1; }
   return buffer + "</main>\n                <a class=\"button alt\" href=\""
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.github : depth0)) != null ? stack1.url : stack1), depth0))
-    + "\" _target=\"blank\">View on GitHub</a>\n            </section>\n        </li>\n";
+    + "\" target=\"_blank\">View on GitHub</a>\n            </section>\n        </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, buffer = "<ul>\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.projects : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
